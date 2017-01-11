31b510828642c8a2359a17c859ae208611e5ab3c
mmihajlovska <marina.mihajlovska@gmail.com>
2017-01-12 00:10:42 +0100

    Developed simple task management project with Angular and Bootstrap

---
+++ tasks/todo.js
@@ -0,0 +1,58 @@
+var app = angular.module('myApp', [ "ngRoute" ]);
+
+app.controller('MainCtrl', function($scope) {
+
+	$scope.tasks = [];
+
+});
+
+app.controller('TasksCtrl', function($scope) {
+
+	$scope.add = function() {
+		$scope.tasks.push({
+			title : $scope.task,
+			completed : false
+		});
+		$scope.task = '';
+	}
+
+	$scope.remainingTasks = function() {
+		var remaining = 0;
+
+		for (var i = 0; i < $scope.tasks.length; i++) {
+			if (!$scope.tasks[i].completed) {
+				remaining++;
+			}
+		}
+
+		return remaining;
+	}
+
+	$scope.remove = function(index) {
+		console.log(index);
+		$scope.tasks.splice(index, 1);
+	}
+
+});
+
+app.config(function($routeProvider) {
+	$routeProvider.when("/", {
+		templateUrl : "tasks.html",
+		controller : "TasksCtrl"
+	}).when("/edit/:ID", {
+		templateUrl : "edit.html",
+		controller : "EditCtrl"
+	}).when("/info", {
+		templateUrl : "info.html",
+		controller : "TasksCtrl"
+	});
+});
+
+app.controller("EditCtrl", function($scope, $routeParams) {
+	$scope.index = $routeParams.ID;
+	$scope.title = $scope.tasks[$scope.index].title;
+
+	$scope.update = function() {
+		$scope.tasks[$scope.index].title = $scope.title;
+	}
+});
